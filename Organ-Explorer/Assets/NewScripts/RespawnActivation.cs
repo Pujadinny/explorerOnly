8d5a9f4abf646a693fee1e14d3132838e5b4d4ce
Pujadinny <theedup456@gmail.com>
2020-04-15 20:23:52 +0200

    OrganExplo
    RespawnFet, MapaFET

---
+++ Organ-Explorer/Assets/NewScripts/RespawnActivation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnActivation : MonoBehaviour
+{
+    public RespawnMoviment resp;
+    private void Start()
+    {
+        resp = GetComponent<RespawnMoviment>();
+    }
+
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        MoureRespawn();
+
+    }
+
+    public void MoureRespawn()
+    {
+        resp.MovimentRespawn();
+    }
+}
+        
+    
+
---
+++ Organ-Explorer/Assets/NewScripts/RespawnMoviment.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnMoviment : MonoBehaviour
+{
+    private Animator animacion;
+    void Start()
+    {
+        animacion = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void MovimentRespawn()
+    {
+        animacion.SetInteger("Status", 1);
+    }
+}
