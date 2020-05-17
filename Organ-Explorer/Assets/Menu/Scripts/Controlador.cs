28051519453ac45b45e3d04d5e1fad7ccd10f034
Pujadinny <theedup456@gmail.com>
2020-05-17 21:40:58 +0200

    OrganExplorer
    Aquesta és la Beta de OrganExplorer

---
+++ Organ-Explorer/Assets/Menu/Scripts/Controlador.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controlador : MonoBehaviour
+{
+  //  public Pasue pausa; //agafo el script de pasue (PAUSA)
+
+    public void ChangeEscena(string nombre) //funico de canviar l'escena
+    {
+
+        print("Benvingut al Organ Explorer");
+        SceneManager.LoadScene(nombre);
+    }
+  public void Exit() //funcio de sortir del Game
+    {
+        print("Adeu fins una altre");
+        Application.Quit();
+
+    }
+
+    
+  //public void Restart()
+   // {
+        
+   // }
+}
