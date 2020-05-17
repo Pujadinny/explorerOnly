a29a06cd54f64a0b488936028a3d8a2c3465e63c
Ignasi8310 <59967561+Ignasi8310@users.noreply.github.com>
2020-05-17 20:29:59 +0200

    hello
    new button mariconetis

---
+++ Organ-Explorer/Assets/NewScripts/Puerta1/DoorOpen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    private Animator anime;
+    void Start()
+    {
+        anime = GetComponent<Animator>();
+    }
+
+
+    public void Movedoor1()
+    {
+
+        anime.SetInteger("Status",1);
+
+    }
+}
---
+++ Organ-Explorer/Assets/NewScripts/Puerta1/PuertaMoving.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertaMoving : MonoBehaviour
+{
+    public  DoorOpen move; 
+
+    private void Start()
+    {
+        move = GetComponent<DoorOpen>();
+    }
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        MoureDoor();
+    }
+
+    public void MoureDoor()
+    {
+
+        move.Movedoor1();
+    }
+}
