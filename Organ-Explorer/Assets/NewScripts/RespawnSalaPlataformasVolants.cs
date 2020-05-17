8dc964bedb31fec2f86002626fa460eb64838250
Daniel-pi <59967588+Daniel-pi@users.noreply.github.com>
2020-05-17 17:56:12 +0200

    Mapa + respawn sala plataformas
    yupyyyyyyy

---
+++ Organ-Explorer/Assets/NewScripts/RespawnSalaPlataformasVolants.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnSalaPlataformasVolants : MonoBehaviour
+{
+    public Transform pleyer;
+    public Transform respawnPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        pleyer.transform.position = respawnPoint.transform.position;
+    }
+}
