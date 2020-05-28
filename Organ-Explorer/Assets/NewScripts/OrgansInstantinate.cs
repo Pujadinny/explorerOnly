aa239ff62fe9565fb3cf952e0e7866ca474f2df8
Pujadinny <theedup456@gmail.com>
2020-05-28 21:51:15 +0200

    OrganExplorer
    Beta Version

--- Organ-Explorer/Assets/NewScripts/OrgansInstantinate.cs
+++ Organ-Explorer/Assets/NewScripts/OrgansInstantinate.cs
@@ -1,58 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class OrgansInstantinate : MonoBehaviour
 {
     //ORGANS 
-    //public GameObject cor;
-    //public GameObject pulmo;
-    //public GameObject instePrim;
-    //public GameObject insteGros;
-    //public GameObject cervell;
-    //public GameObject liver;
+    public GameObject cor;
+    public GameObject pulmo;
+    public GameObject instePrim;
+    public GameObject insteGros;
+    public GameObject cervell;
+    public GameObject liver;
+    
 
-    //Boton organos
-   // public GameObject BotonCor;
-   // public GameObject BotonPulmon;
-  //  public GameObject BotonRunyo;
-   // public GameObject BotonIntesGros;
-    //public GameObject BotonIntesPrim;
-    //public GameObject BotonEstomac;
-    //public GameObject BotonCervell;
+   //Boton organos
+    public Button butonOrgan;
+   
 
     //boolena
-   // bool corbool;
+   bool organs;
 
     //POSI organs
-    //public Transform posiCor;
-    //public Transform posiPulmo;
-    //public Transform posiIntestiPrim;
-    //public Transform posiIntestiGros;
-    //public Transform posiCervell;
-    //public Transform posiLiver;
-
+     public Transform posiCor;
+     public Transform posiPulmo;
+     public Transform posiIntestiPrim;
+     public Transform posiIntestiGros;
+     public Transform posiCervell;
+     public Transform posiLiver;
 
 
     
     
         
     
+
+
+
+
     // Update is called once per frame
-    //void Update()
-    //{
-        //if (Input.GetKeyDown(KeyCode.K))
-        //{
+   public void Patata()
+    {
+        
+        
 
-            //Instantiate(cor, posiCor.position, Quaternion.identity);
-            //Instantiate(instePrim, posiIntestiPrim.position, Quaternion.identity);
-            //Instantiate(insteGros, posiIntestiGros.position, Quaternion.identity);
-            //Instantiate(cervell, posiCervell.position, Quaternion.identity);
-            //Instantiate(liver, posiLiver.position, Quaternion.identity);
+            Instantiate(cor, posiCor.position, Quaternion.identity);
+            Instantiate(instePrim, posiIntestiPrim.position, Quaternion.identity);
+            Instantiate(insteGros, posiIntestiGros.position, Quaternion.identity);
+            Instantiate(cervell, posiCervell.position, Quaternion.identity);
+            Instantiate(pulmo, posiPulmo.position, Quaternion.identity);
+            Instantiate(liver, posiLiver.position, Quaternion.identity);
 
-        //}
 
-    //}
+
+    }
 }
    
         
