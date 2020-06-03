62706efa657264d2f91bd2bd3f6ec62f0c387b61
Pujadinny <theedup456@gmail.com>
2020-06-03 19:48:19 +0200

    OrganExplorer
    FINAL HECHO

---
+++ Organ-Explorer/Assets/NewScripts/FinalDoorActivation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalDoorActivation : MonoBehaviour
+{
+    public FinalDoorMoviment portafinal;
+
+
+     void Start()
+    {
+        portafinal = GetComponent<FinalDoorMoviment>();
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        MourePortaFinal();
+
+    }
+
+    public void MourePortaFinal()
+    {
+       
+        portafinal.MoviPortaFinal();
+
+    }
+
+
+
+}
---
+++ Organ-Explorer/Assets/NewScripts/FinalDoorMoviment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalDoorMoviment : MonoBehaviour
+{
+    private Animator finanima;
+    void Start()
+    {
+        finanima = GetComponent<Animator>();
+    }
+
+
+    public void OnTriggerEnter(Collider finaltriger)
+    {
+        MoviPortaFinal();
+    }
+    public void MoviPortaFinal()
+    {
+        finanima.SetInteger("Status", 1);
+    }
+}
