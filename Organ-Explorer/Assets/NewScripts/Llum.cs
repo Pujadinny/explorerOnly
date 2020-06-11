7207d8bbe794757027623e3df2c436fcc6c9cf69
Pujadinny <theedup456@gmail.com>
2020-06-11 20:34:11 +0200

    OrganExplorer
    LLum Activada

---
+++ Organ-Explorer/Assets/NewScripts/Llum.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Llum : MonoBehaviour
+{
+    public  GameObject llum;
+    public bool Onllum;
+
+    public void Start()
+    {
+        llum.gameObject.GetComponent<Light>();
+        Onllum = true;
+    }
+
+    public void Luz()
+    {
+        if (Onllum == false)
+        {
+
+            llum.SetActive(true);
+        }
+        else  
+        {
+            llum.SetActive(false);
+        }
+        
+    }
+    
+    
+
+
+}
+    // Update is called once per frame
+
